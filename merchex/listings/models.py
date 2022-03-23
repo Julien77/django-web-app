884404d5146d20da70f3bd537c92e0eb86e31f09
Julien77 <julien.marcel.dev@protonmail.com>
2022-03-23 15:32:31 +0100

    add fiels record company to db

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -17,6 +17,7 @@ class Band(models.Model):
 	active = models.fields.BooleanField(default=True)
 	official_homepage = models.fields.URLField(null=True, blank=True)
 	like_new = models.fields.BooleanField(default=False)
+	record_company = models.fields.CharField(max_length=100, null=True, blank=True)
 
 	def __str__(self):
 		return f"{self.name}"
